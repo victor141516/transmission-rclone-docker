abc8a86edc3222f72609061f32dba1cc04ece3a7
Víctor <yomellamovictor@gmail.com>
2019-01-28 02:21:13 +0100

    Update on_download_finish.sh

--- on_download_finish.sh
+++ on_download_finish.sh
@@ -1,2 +1,2 @@
 #!/bin/sh	
-rclone copy --config /rclone/rclone.conf /downloads/complete $RCLONE_REMOTE && python /remove_until_size.py /downloads/complete $MAX_DOWNLOADS_DIR_SIZE
+rclone copy --config /rclone/rclone.conf /downloads/complete $RCLONE_REMOTE && python3 /remove_until_size.py /downloads/complete $MAX_DOWNLOADS_DIR_SIZE
