be2478dfd768f84d84d897254dad2610a7cb2b9c
Víctor <yomellamovictor@gmail.com>
2019-06-16 04:30:26 +0200

    Update on_download_finish.sh

--- on_download_finish.sh
+++ on_download_finish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh	
 mkdir -p /logs/rclone && \
-rclone copy -v --config /rclone/rclone.conf /downloads/complete $RCLONE_REMOTE > "/logs/rclone/$(date +'%d_%m_%Y__%H_%M_%S').log" 2>&1 && \
+rclone copy --ignore-existing -v --config /rclone/rclone.conf /downloads/complete $RCLONE_REMOTE > "/logs/rclone/$(date +'%d_%m_%Y__%H_%M_%S').log" 2>&1 && \
 python3 /remove_until_size.py /downloads/complete $MAX_DOWNLOADS_DIR_SIZE
